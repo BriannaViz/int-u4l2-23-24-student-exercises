f2699b02a6770cf970a95f70f1fa6bbf27b76b18
BriannaViz <146958013+BriannaViz@users.noreply.github.com>
2024-03-20 13:52:48 +0000

    yea no

--- concatenation.js
+++ concatenation.js
@@ -2,18 +2,18 @@ let submitButton = document.querySelector(".submit");
 let results = document.querySelector(".results");
 
 submitButton.addEventListener("click", function() {
-    results.style.display = "block";
-    let myName = document.querySelector(".my-name").value;
-    let mood = document.querySelector(".mood").value;
-    let food = document.querySelector(".food-name").value;
-    let birthYear = document.querySelector(".year").value;
-    let age = 2023 - birthYear; // update to 2024, if necessary
-    // 1. Declare a variable to save the value of the city the user is from.
+  results.style.display = "block";
+  let myName = document.querySelector(".my-name").value;
+  let mood = document.querySelector(".mood").value;
+  let food = document.querySelector(".food-name").value;
+  let birthYear = document.querySelector(".year").value;
+  let age = 2023 - birthYear; // update to 2024, if necessary
+  // 1. Declare a variable to save the value of the city the user is from.
+  let city = document.querySelector(".city").value
+  // 2. Below write your sentences! 
+  // - Have them show up in the div with class "results".
+  // - Example paragraph: "My name is (myName). I'm feeling (mood) and I absolutely lovveeee eating (food) 😍..."
+  // - Try placing your sentences into <p> or <li> tags.
 
-    // 2. Below write your sentences! 
-    // - Have them show up in the div with class "results".
-    // - Example paragraph: "My name is (myName). I'm feeling (mood) and I absolutely lovveeee eating (food) 😍..."
-    // - Try placing your sentences into <p> or <li> tags.
-
-
+results.innerHTML = "Hi " + myName + ", how are you? I see you're " + mood + " today, that's unexpected from you. " + food + " is my favorite food too! I see you're a " + birthYear + " baby, honestly you're really old... You live in " + city + ", that's not far from where I live..." ;
 });
